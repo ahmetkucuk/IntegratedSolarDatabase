0663dc865aa8b9e7f71c5008ffce7370f3d58fee
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2016-01-02 07:11:56 -0500

    heroku port

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	_ "solardatabase/routers"
 	"github.com/astaxie/beego"
 //	"solardatabase/dao"
+	"strconv"
+	"os"
 )
 
 func main() {
-
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.HttpPort = port
+	}
 //	dao.OpenDBConnection()
 	beego.Run()
 }
