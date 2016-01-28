0d8ff2c8ede0333729f49d641ca008ff0a06e71c
Ahmet Kucuk <ahmetkucuk92@gmail.com>
2016-01-28 00:11:14 -0500

    change to make it work with new version

--- controllers/EventController.go
+++ controllers/EventController.go
@@ -15,17 +15,17 @@ type EventController struct {
 func (this *EventController) ListEvents() {
 	res := struct{ Tasks []*models.Event }{dao.GetAllEvents()}
 	this.Data["json"] = res
-	this.ServeJson()
+	this.ServeJSON()
 }
 
 func (this *EventController) ListEventsByRange() {
 	request, _ := models.CreateEventByTimeRangeRequest(this.Ctx.Input)
 	this.Data["json"] = dao.EventsByTimeRange(request)
-	this.ServeJson()
+	this.ServeJSON()
 }
 
 func (this *EventController) TemporalQuery() {
 	request, _ := models.CreateTemporalRequest(this.Ctx.Input)
 	this.Data["json"] = services.EventsByTimeFilter(request)
-	this.ServeJson()
+	this.ServeJSON()
 }
